206ae6083264835cec5363027651acd4e8af13c9
korguchi <korguchi@gmail.com>
2023-12-18 02:57:31 +0900

    Bug fix

--- meldataset.py
+++ meldataset.py
@@ -85,28 +85,6 @@ def mel_spectrogram(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin,
 
 def get_dataset_filelist(a):
     with open(a.input_training_file, 'r', encoding='utf-8') as fi:
-        training_files = [os.path.join(a.input_wavs_dir, x.split('|')[0] + '.wav')
-                          for x in fi.read().split('\n') if len(x) > 0]
-        print("first training file: {}".format(training_files[0]))
-
-    with open(a.input_validation_file, 'r', encoding='utf-8') as fi:
-        validation_files = [os.path.join(a.input_wavs_dir, x.split('|')[0] + '.wav')
-                            for x in fi.read().split('\n') if len(x) > 0]
-        print("first validation file: {}".format(validation_files[0]))
-
-    list_unseen_validation_files = []
-    for i in range(len(a.list_input_unseen_validation_file)):
-        with open(a.list_input_unseen_validation_file[i], 'r', encoding='utf-8') as fi:
-            unseen_validation_files = [os.path.join(a.list_input_unseen_wavs_dir[i], x.split('|')[0] + '.wav')
-                                for x in fi.read().split('\n') if len(x) > 0]
-            print("first unseen {}th validation fileset: {}".format(i, unseen_validation_files[0]))
-            list_unseen_validation_files.append(unseen_validation_files)
-
-    return training_files, validation_files, list_unseen_validation_files
-
-
-def get_dataset_simplefilelist(a):
-    with open(a.input_training_file, 'r', encoding='utf-8') as fi:
         training_files = [x+'.wav' for x in fi.read().split('\n') if len(x) > 0]
         print("first training file: {}".format(training_files[0]))
 
